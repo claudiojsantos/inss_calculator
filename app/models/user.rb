8c6007651bc4f8a5e2fddf3d9d8203321c9c4a9c
Cláudio Santos <cljos@hotmail.com>
2024-12-23 16:31:28 -0300

    refactor(model): improviment password validation and add twice methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   validates :email, presence: { message: 'Por favor, insira um endereço de e-mail válido.' }
   validates :email, uniqueness: { message: 'Este e-mail já está em uso.' }
   validates :password, presence: { message: 'Por favor, digite a senha.' }, if: :password_required?
+
+  def valid_password?(password)
+    BCrypt::Password.new(password_digest) == password
+  end
+
+  def password_required?
+    new_record? || password.present?
+  end
 end
