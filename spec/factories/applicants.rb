13f7b307844984c0c1ba9adb24d8c170586b3045
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:39:04 -0300

    refactor(factories): update values of attributes on Applicant

--- spec/factories/applicants.rb
+++ spec/factories/applicants.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :applicant do
-    
+    name { Faker::Name.name }
+    document { Faker::IdNumber.brazilian_citizen_number }
+    birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    salary { Faker::Number.decimal(l_digits: 4, r_digits: 2) }
+    inss_discount { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
   end
 end
