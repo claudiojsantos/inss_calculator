62078a58df66d20d3453f2fd90f84626d323ac4e
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:37:25 -0300

    refactor(model): add associations, nested attributes and validations on Applicant class

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -1,2 +1,12 @@
 class Applicant < ApplicationRecord
+  has_many :addresses, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+
+  accepts_nested_attributes_for :addresses, :contacts, allow_destroy: true
+
+  validates :name, presence: true
+  validates :document, presence: true, uniqueness: true
+  validates :birth_date, presence: true
+  validates :salary, numericality: { greater_than: 0 }
+  validates :inss_discount, numericality: { greater_than_or_equal_to: 0 }
 end
