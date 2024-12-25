e41c957d7e3158acfafba26012d5aed696fe15f9
Cláudio Santos <cljos@hotmail.com>
2024-12-25 19:18:00 -0300

    feat(helper): format mask cpf helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def format_cpf(cpf)
+    cpf.to_s.gsub(/\D/, '')
+       .rjust(11, '0')
+       .gsub(/(\d{3})(\d{3})(\d{3})(\d{2})/, '\1.\2.\3-\4') # Formata como XXX.XXX.XXX-XX
+  end
 end
