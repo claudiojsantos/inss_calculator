1e63a058cfc6aadd2312b4019f8593d338cf04e0
Cláudio Santos <cljos@hotmail.com>
2024-12-25 19:50:40 -0300

    chore(seed): add User and applicant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!([
+  { name: 'Admin', email: 'admin@test.com', password: '12345678' }
+])
+
+10.times do
+  applicant = Applicant.create!(
+    name: Faker::Name.name,
+    document: Faker::IdNumber.unique.brazilian_citizen_number,
+    birth_date: Faker::Date.birthday(min_age: 18, max_age: 65),
+    salary: Faker::Number.decimal(l_digits: 4, r_digits: 2),
+    inss_discount: 0.0
+  )
+
+  applicant.addresses.create!(
+    address: Faker::Address.street_name,
+    number: Faker::Address.building_number,
+    neighborhood: Faker::Address.community,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip_code: Faker::Address.zip_code
+  )
+
+  rand(1..5).times do
+    applicant.contacts.create!(
+      contact_type: Contact.contact_types.keys.sample,
+      value: Faker::PhoneNumber.phone_number
+    )
+  end
+end
