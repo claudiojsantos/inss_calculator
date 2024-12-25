ec56c48fafa6244a384a4109cc1c0a5647507f23
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:52:57 -0300

    refactor(model): add association and validations on Contact class

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  belongs_to :applicant
+
+  enum :contact_type, residential: 0, mobile: 1, commercial: 2, whatsapp: 3, email: 4
+
+  validates :value, presence: true
 end
