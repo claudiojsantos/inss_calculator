84571552087c06c977781f6c86b6c0dd47f94456
Cláudio Santos <cljos@hotmail.com>
2024-12-25 18:08:48 -0300

    feat(:controller): add authenticate and validate methods on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,28 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  before_action :authenticate_request
+
+  helper_method :current_user, :user_signed_in?
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_request
+    if request.format.json?
+      @current_user = AuthorizeApiRequest.call(request.headers).result
+      render json: { error: 'Acesso não autorizado' }, status: :unauthorized unless @current_user
+    else
+      unless user_signed_in?
+        flash[:alert] = 'Acesso não permitido.'
+        redirect_to login_path
+      end
+    end
+  end
+
+  def user_signed_in?
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id].present?
+    @current_user.present?
+  end
 end
