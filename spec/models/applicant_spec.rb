d16a0d6cd9be4aed11b5994bbd4d0a243a9e2b28
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:42:45 -0300

    test(model): add association, validations and factories on Applicant

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Applicant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:document) }
+    it { should validate_uniqueness_of(:document) }
+    it { should validate_presence_of(:birth_date) }
+    it { should validate_numericality_of(:salary).is_greater_than(0) }
+    it { should validate_numericality_of(:inss_discount).is_greater_than_or_equal_to(0) }
+  end
+
+  describe 'associations' do
+    it { should have_many(:addresses).dependent(:destroy) }
+    it { should have_many(:contacts).dependent :destroy }
+  end
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      applicant = build(:applicant)
+      expect(applicant).to be_valid
+    end
+  end
 end
