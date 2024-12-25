c9dd21d7771d0ba18467ddae6833b20ec214dc85
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:48:14 -0300

    refactor(factories): update values of attributes on Address

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :address do
-    
+    address { Faker::Address.street_name }
+    number { Faker::Address.building_number }
+    neighborhood { Faker::Address.community }
+    city { Faker::Address.city }
+    state { Faker::Address.state_abbr }
+    zip_code { Faker::Address.zip_code }
+    applicant
   end
 end
