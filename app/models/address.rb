364834d90834d158d5ef8b3150245720f5a65eba
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:47:46 -0300

    refactor(model): add associations and validations on Address class

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :applicant
+
+  validates :address, presence: true
+  validates :number, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :city, presence: true
+  validates :zip_code, presence: true
 end
