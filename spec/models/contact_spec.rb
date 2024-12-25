ed1bef076e0f3071d9662b72ff0911e4fb52923d
Cláudio Santos <cljos@hotmail.com>
2024-12-25 20:53:39 -0300

    test(model): add association, validations and factories on Contact

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should belong_to(:applicant) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:value) }
+
+    it 'defines the correct enum for contact_type' do
+      expect(Contact.contact_types).to eq({
+                                            'residential' => 0,
+                                            'mobile' => 1,
+                                            'commercial' => 2,
+                                            'whatsapp' => 3,
+                                            'email' => 4
+                                          })
+    end
+  end
+
+  describe 'factory' do
+    it 'has a valid factory' do
+      contact = build(:contact)
+      expect(contact).to be_valid
+    end
+  end
 end
