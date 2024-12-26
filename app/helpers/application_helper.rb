3a3848652fe2036323c3f26a936425d930fab6ec
Cláudio Santos <cljos@hotmail.com>
2024-12-26 15:48:39 -0300

    refactor(helper): add uf method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
        .rjust(11, '0')
        .gsub(/(\d{3})(\d{3})(\d{3})(\d{2})/, '\1.\2.\3-\4') # Formata como XXX.XXX.XXX-XX
   end
+
+  def uf
+    [
+      [ 'Acre', 'AC' ], [ 'Alagoas', 'AL' ], [ 'Amapá', 'AP' ], [ 'Amazonas', 'AM' ], [ 'Bahia', 'BA' ],
+      [ 'Ceará', 'CE' ], [ 'Distrito Federal', 'DF' ], [ 'Espírito Santo', 'ES' ], [ 'Goiás', 'GO' ],
+      [ 'Maranhão', 'MA' ], [ 'Mato Grosso', 'MT' ], [ 'Mato Grosso do Sul', 'MS' ], [ 'Minas Gerais', 'MG' ],
+      [ 'Pará', 'PA' ], [ 'Paraíba', 'PB' ], [ 'Paraná', 'PR' ], [ 'Pernambuco', 'PE' ], [ 'Piauí', 'PI' ],
+      [ 'Rio de Janeiro', 'RJ' ], [ 'Rio Grande do Norte', 'RN' ], [ 'Rio Grande do Sul', 'RS' ],
+      [ 'Rondônia', 'RO' ], [ 'Roraima', 'RR' ], [ 'Santa Catarina', 'SC' ], [ 'São Paulo', 'SP' ],
+      [ 'Sergipe', 'SE' ], [ 'Tocantins', 'TO' ]
+    ]
+  end
 end
