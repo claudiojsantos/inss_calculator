63a8501b08b6b37629ca5f3d1245a29955ff79ca
Cláudio Santos <cljos@hotmail.com>
2024-12-26 15:55:22 -0300

    refactor(model): add callback

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -9,4 +9,12 @@ class Applicant < ApplicationRecord
   validates :birth_date, presence: true
   validates :salary, numericality: { greater_than: 0 }
   validates :inss_discount, numericality: { greater_than_or_equal_to: 0 }
+
+  after_create :enqueue_inss_discount_calculation, if: -> { inss_discount.nil? }
+
+  private
+
+  def enqueue_inss_discount_calculation
+    InssCalculatorJob.perform_later(self.salary, self.id)
+  end
 end
