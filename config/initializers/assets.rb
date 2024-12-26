77ea9efaae1cd6a237b8236a1c340ca660520702
Cláudio Santos <cljos@hotmail.com>
2024-12-26 15:47:38 -0300

    chore(config): set path assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+Rails.application.config.assets.version = '1.0'
+
+Rails.application.config.assets.configure do |env|
+  env.register_mime_type 'application/javascript', extensions: [ '.js' ]
+end
+
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
