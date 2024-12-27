685d377b04c8fbfe6cea7603618e20ed41ab3553
Cláudio Santos <cljos@hotmail.com>
2024-12-27 01:40:02 -0300

    test(factory): update value attribute

--- spec/factories/applicants.rb
+++ spec/factories/applicants.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
     document { Faker::IdNumber.brazilian_citizen_number }
     birth_date { Faker::Date.birthday(min_age: 18, max_age: 65) }
     salary { Faker::Number.decimal(l_digits: 4, r_digits: 2) }
-    inss_discount { Faker::Number.decimal(l_digits: 3, r_digits: 2) }
+    inss_discount { 0.0 }
   end
 end
