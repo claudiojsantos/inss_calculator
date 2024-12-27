02f3a0417373dda5a9f227a41ed8193b7fd8b1d4
Cláudio Santos <cljos@hotmail.com>
2024-12-27 01:35:30 -0300

    refactor(config): reconfigure path server

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  redis_url = ENV.fetch('REDIS_URL') { 'redis://redis:6379/0' }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0' }
+  redis_url = ENV.fetch('REDIS_URL') { 'redis://redis:6379/0' }
+  config.redis = { url: redis_url }
 end
